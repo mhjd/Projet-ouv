43b3c1208e4f004f4d945ebaf4de4f27618c6b8d
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2023-10-17 15:40:02 +0200

    Essai sur les décompositions

--- echauffement.ml
+++ echauffement.ml
@@ -16,3 +16,17 @@ let list_decapit l =
   match l with
   | [] -> []
   | hd::tl -> tl;;
+
+let to_bool (x:int64):bool = match x with zero -> false | _ -> true;;
+
+let rec decompose_entier64 (hd : int64) : bool list = 
+  match hd with 
+  | 0L -> [false]
+  | 1L -> [true]
+  | _ -> let bit = unsigned_rem hd 2L and suite = unsigned_div hd 2L in (to_bool bit)::(decompose_entier64 suite);;
+
+let rec decomposition (x : entier_precis) : bool list =
+  match x with 
+  | [] -> []
+  | hd::tl -> (decompose_entier64 hd)@(decomposition tl)
+
