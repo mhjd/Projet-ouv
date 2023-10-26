a1f2d0814272efbccf976df5ac42cb78b5fe53c1
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2023-10-27 00:30:58 +0200

    légère modif sur cons

--- proposition2.ml
+++ proposition2.ml
@@ -31,7 +31,7 @@ let cons_arbre (t : bool list) : arbre_decision =
     | l -> let nb_feuilles = (List.length t) / 2 in
         let sous_arbre_droit = cons_arbre_head (completion l nb_feuilles) (dpth+1) in
         let sous_arbre_gauche = cons_arbre_head (suite_liste l nb_feuilles) (dpth+1) in
-        Noeud(dpth, sous_arbre_droit , sous_arbre_gauche)
+        Noeud(dpth, ref sous_arbre_droit , ref sous_arbre_gauche)
   in cons_arbre_head t 1
 (* Exemple de test, repris de l'énoncé :
 cons_arbre (table 25899 16) ;;
