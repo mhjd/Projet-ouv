7774358735bf1aee94c403524e1e7d4e47f945fd
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2023-10-30 21:19:55 +0100

    Meilleur script

--- test.sh
+++ test.sh
@@ -1,4 +1,14 @@
-make
-./dot.exe
-dot -Tpng arbre_comp.dot -o Images/arbre_compresse.png
-dot -Tpng arbre_non_comp.dot -o Images/arbre_non_compresse.png
+#!/bin/bash
+
+    if (make > trace_compilation.txt) then 
+
+    echo "La compilation a reussi ! On peut lancer l'exécution"
+
+    ./dot.exe
+
+
+    dot -Tpng arbre_comp.dot -o Images/arbre_compresse.png
+    dot -Tpng arbre_non_comp.dot -o Images/arbre_non_compresse.png
+    else 
+        echo "Attention ! Mauvaise version de OCaml ! Il faut la 4.14 !"
+    fi
